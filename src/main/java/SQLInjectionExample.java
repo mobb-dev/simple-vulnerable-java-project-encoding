1a50bc2addfbe55da1cf43788400cc6e8d50f5d7
Kirill Efimov <kirill89@gmail.com>
2024-11-29 17:19:14 +0100

    use ISO-8859-1

--- src/main/java/SQLInjectionExample.java
+++ src/main/java/SQLInjectionExample.java
@@ -2,12 +2,15 @@ import java.sql.*;
 
 public class SQLInjectionExample {
     public static void main(String[] args) throws SQLException {
+        // ó
         String userInputA = args[1];
-
+        // ó
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db", "root", "root");
-
+        // ó
         String query = "SELECT * FROM users WHERE username = '" + userInputA + "';";
+        // ó
         Statement stmt = con.createStatement();
+        // ó
         ResultSet rs = stmt.executeQuery(query);
 
         while (rs.next()) {
